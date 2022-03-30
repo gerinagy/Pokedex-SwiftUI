83419e2950b58106a507747edb662890481d24dc
Nagy Gergő <nagy.gergi6@gmail.com>
2022-03-31 00:11:41 +0200

    feat: Create pokemon views, fetch pokemons,

---
+++ Pokedex/Views/PokemonImage.swift
@@ -0,0 +1,47 @@
+//
+//  PokemonImage.swift
+//  Pokedex
+//
+//  Created by Gergo Nagy on 3/30/22.
+//
+
+import SwiftUI
+
+struct PokemonImage: View {
+    var imageLink = ""
+    @State private var pokemonSprite = ""
+    
+    var body: some View {
+        AsyncImage(url: URL(string: pokemonSprite))
+            .frame(width: 75, height: 75)
+            .onAppear {
+                let loadedData = UserDefaults.standard.string(forKey: imageLink)
+                
+                if loadedData == nil {
+                    getSprite(url: imageLink)
+                    UserDefaults.standard.set(imageLink, forKey: imageLink)
+                    print("New url! Caching.")
+                } else {
+                    getSprite(url: loadedData!)
+                    print("Useing cached url.")
+                }
+            }
+            .clipShape(Circle())
+            .foregroundColor(Color.gray.opacity(0.60))
+    }
+    
+    func getSprite(url: String) {
+        var tempSprite: String?
+        
+        PokemonSelectedApi().getData(url: url) { sprite in
+            tempSprite = sprite.front_default
+            self.pokemonSprite = tempSprite ?? "placeholder" // Image
+        }
+    }
+}
+
+struct PokemonImage_Previews: PreviewProvider {
+    static var previews: some View {
+        PokemonImage()
+    }
+}
